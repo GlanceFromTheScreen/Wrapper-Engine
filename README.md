b0acc1ccc0f90fe548a488c4a27bc331878f6477
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2022-12-08 22:43:46 +0300

    readme + main unit test correct

---
+++ README.md
@@ -0,0 +1,6 @@
+Wrapper and Engine joint usage: 
+1) A pair of class instance and class method is taken. 
+2) These pair is wrapping in Wrapper. 
+3) Wrapper instances push into Engine collection. 
+4) Engine class has "execute" method to execute method from (1)
+So, we just need to specify our execution by command name and parameter list to get the correct answer.
--- main.cpp
+++ main.cpp
@@ -24,10 +24,11 @@ public:
 int main() {
 
     Subject subj;
-    Wrapper wrapper(&subj,&Subject::f3, {{"arg1",1}, {"arg2", 2}});
+    Wrapper wrapper(&subj,&Subject::f3, {{"arg1",0}, {"arg2", 0}});
     Engine engine;
     engine.register_command(&wrapper, "command1");
-    engine.execute("command1", {{"arg1",6}, {"arg2", 7}});
+    std::cout << engine.execute("command1",{{"arg1",4},{"arg2",5}}) << std::endl;
+
 
     OneMoreSubject subj2;
     Wrapper wrapper2(&subj2, &OneMoreSubject::f3, {{"_1",0}, {"_2", 0}, {"_3", 0}});
@@ -35,6 +36,8 @@ int main() {
     engine2.register_command(&wrapper2, "cmd");
     engine2.register_command(&wrapper2, "cmd2");
     engine2.execute("cmd2", {{"_1",1}, {"_2", 2}});
+
+
 
     return 0;
 }
